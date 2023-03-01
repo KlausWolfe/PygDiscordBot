b54446d7b6cd8ee67e2e9dcf1eef0f254793f21a
ausboss <austin.haspany@gmail.com>
2023-03-01 13:44:53 -0600

    catch for empty messages

--- cogs/messagehandler.py
+++ cogs/messagehandler.py
@@ -50,9 +50,10 @@ class ListenerCog(commands.Cog, name="listener"):
             if await self.has_image_attachment(message):
                 image_response = await self.bot.get_cog("image_caption").image_comment(message, message.content)
                 response = await self.bot.get_cog("chatbot").chat_command(message, image_response)
-                async with message.channel.typing():
-                    await asyncio.sleep(1)  # Simulate some work being done
-                    await message.reply(response)
+                if response:
+                    async with message.channel.typing():
+                        await asyncio.sleep(1)  # Simulate some work being done
+                        await message.reply(response)
             else:
                 # No image. Normal text response
                 response = await self.bot.get_cog("chatbot").chat_command(message, message.content)
